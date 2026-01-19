865efc759e10d2a294df0c0869c329e75928d992
Souvik Chakraborty <Souvikc3030@gmail.com>
2026-01-19 17:57:05 +0530

    multer & minio has been fixed to store images with metadata

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Architecute
 
 ![Architecute](./assets/images/architecture.svg)
+
+---
+
+## Require Fix
+- [ ] Multer file upload name in the upload/ dir
+
+---
+
+## References
+- [Usage of multer](https://betterstack.com/community/guides/scaling-nodejs/multer-in-nodejs/)
+- [Multer - Stack Overflow](https://stackoverflow.com/questions/15772394/how-to-upload-display-and-save-images-using-node-js-and-express)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Schema, model, connect } from 'mongoose';
 import dotenv from 'dotenv'
-// import * as Minio from 'minio'
+import * as Minio from 'minio'
 import { User } from "./db";
 import multer from "multer";
 import path from "path";
@@ -9,26 +9,30 @@ import { fileURLToPath } from "url";
 
 const upload = multer({ dest: 'uploads/' })
 // const __filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(__filename);
+// const dirname = path.dirname(__filename);
+
+// const filePath = "../uploads/"
+// const filename = path.basename(filePath)
 
 dotenv.config({ path: './.env' })
-// const minioClient = new Minio.Client({
-//   endPoint: 'play.min.io',
-//   port: 9000,
-//   useSSL: true,
-//   accessKey: 'test',
-//   secretKey: 'test123',
-// })
+
+// Console: https://play.min.io:9443/
+const minioClient = new Minio.Client({
+    endPoint: 'play.min.io',
+    port: 9000,
+    useSSL: true,
+    accessKey: 'Q3AM3UQ867SPQQA43P2F',
+    secretKey: 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+})
 
 // set up multer storage
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/uploads')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
+    destination: function (req, file, cb) {
+        cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
 });
 
 const app = express();
@@ -39,7 +43,7 @@ let DB_URL: string | undefined = process.env.DATABASE_URL;
 app.use(express.json())
 
 if (DB_URL == undefined) {
-    DB_URL = "http://localhost:27017/testDB";
+    DB_URL = "mongodb://localhost:27017/testDB";
 }
 
 async function connectDB(conn_string: string) {
@@ -80,28 +84,43 @@ app.post("/test", async (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send(`
-    <h1>File Upload Demo</h1>
-    <form action="/upload/images" method="post" enctype="multipart/form-data">
-        <input type="file" name="uploadedFile" />
-        <button type="submit">Upload</button>
-    </form>
-  `);
+    res.send(`
+             <h1>File Upload Demo</h1>
+             <form action="/upload/images" method="post" enctype="multipart/form-data">
+             <input type="file" name="uploadedFile" />
+             <button type="submit">Upload</button>
+             </form>
+             `);
 });
 
-app.post("/upload/images", upload.single('uploadedFile'), (req, res) => {
-  console.log(req.file); // Contains file info
-  if (req.file != undefined) {
-      res.send(`File uploaded successfully: ${req.file.filename}`);
-  }
-  else {
-      res.send("Error while uploading images")
-  }
-});
+const bucket = "blue-ledger-objects";
 
-// app.post('/upload', upload.single("file"), (req, res) => {
-//
-// });
+app.post("/upload/images", upload.single('uploadedFile'), async (req, res) => {
+    console.log(req.file); // Contains file info
+    if (req.file != undefined) {
+        // finename
+        const sourceFileName = req.file.originalname;
+        // file path
+        const sourceFilePath = req.file.path;
+        const exists = await minioClient.bucketExists(bucket)
+        if (exists) {
+            console.log("Bucket " + bucket + " exists");
+        } else {
+            await minioClient.makeBucket(bucket, "us-east-1");
+            console.log("Bucket " + bucket + " created in us-east-1.");
+        }
+
+        let metaData = {
+            "Content-Type": "image/jpg"
+        }
+        await minioClient.fPutObject(bucket, req.file.originalname, sourceFilePath, metaData)
+        console.log("File " + sourceFilePath + " upload as object " + req.file.originalname + " in bucket " + bucket);
+        res.send(`File uploaded successfully: ${req.file.originalname}`);
+    }
+    else {
+        res.send("Error while uploading images")
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
